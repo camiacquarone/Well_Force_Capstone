5c0c706fb991ed5ceee7240427cf82536f72d38a
joshuadonatien <joshuadontien@gmail.com>
2025-07-08 11:14:05 -0700

    WIP setup

---
+++ frontend/src/index.jsx
@@ -0,0 +1,12 @@
+import React, { StrictMode } from "react";
+import ReactDOM from 'react-dom/client'
+import { StrictMode } from "react";
+import App from "./App/App"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App/>
+
+    </StrictMode>
+)
