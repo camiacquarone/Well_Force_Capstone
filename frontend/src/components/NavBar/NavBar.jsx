7e9d9e649f19cab41ed8053c05fe927266397925
joshuadonatien <joshuadontien@gmail.com>
2025-07-14 16:13:54 -0700

    WIP sidebar1

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+function NavBar {
+    <div className="wrapper">
+        <div className="link_to_pages">
+            <Link>
+            </Link>
+        </div>
+
+
+    </div>
+}
