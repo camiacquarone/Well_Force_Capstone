a11e3aec90b3425937d886b5826083b27f112716
janinejimen <jimenezj9@utexas.edu>
2025-07-18 10:57:11 -0700

    setup the backend and frontend to send over user informationnwhen creating profile

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-console.log("API key loaded:", process.env.OPENAI_API_KEY);
 
 const chatRoutes = require("./routes/chat.js");
 const userRoutes = require("./routes/user-routes.js");
@@ -11,14 +10,15 @@ const mealRoutes = require("./routes/meal-routes.js");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/users", userRoutes);
-app.use("/api", chatRoutes); // This mounts /api/chat
-app.use("/api/meals", mealRoutes); // This mounts api/meals
+app.use("/api/users", userRoutes);
+app.use("/api", chatRoutes); 
+app.use("/api/meals", mealRoutes); 
+app.use("/api/snacks", snackRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is working.");
