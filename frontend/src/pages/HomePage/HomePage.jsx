7f50d43a9c93dd6c59b2d42863c90c9d6d7fd564
janinejimen <jimenezj9@utexas.edu>
2025-07-19 22:02:42 -0700

    home page displays information!

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -15,22 +15,17 @@ import NavBar from "../../components/NavBar/NavBar";
 import "../HomePage/HomePage.css";
 
 const HomePage = ({ user, setUser }) => {
-  // useAuth hook from Clerk to get the current session token and user info
-  const { userId, isLoaded, isSignedIn, user } = useAuth();
-  const [isOpen, setIsOpen] = useState(false);
-
-  // useEffect hook to update Clerk user ID when auth state changes
   useEffect(() => {
     console.log("HomePage.jsx - Received user prop:", user);
   }, [user]);
 
   return (
     <div>
+      <h2 className="welcome-user">
+        Welcome, {user ? user.name || "Friend" : "Guest"}!
+      </h2>
       <div className="App p-4 bg-gray-100 min-h-screen font-inter flex flex-col items-center justify-center">
         <header className="App-header text-center mb-8 bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-          <h1 className="welcome-user">
-            Welcome, {user ? user.name || "Friend" : "Guest"}!
-          </h1>
           <div className="mt-4 flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4">
             <SignedOut>
               <SignInButton
