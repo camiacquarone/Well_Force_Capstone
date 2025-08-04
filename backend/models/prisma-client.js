d45c4606b5be0a9596ed48181617f6b8695f9dac
joshuadonatien <joshuadontien@gmail.com>
2025-08-04 15:29:51 -0700

    Changing backend model to prisma client to run the backend

--- backend/models/prisma-client.js
+++ backend/models/prisma-client.js
@@ -1,4 +1,4 @@
 // models/prisma-client.js
-const { PrismaClient } = require('../generated/prisma'); // Go up one level (backend/) then into generated/prisma
+const { PrismaClient } = require('@prisma/client'); // Go up one level (backend/) then into generated/prisma
 const prisma = new PrismaClient();
 module.exports = prisma;
